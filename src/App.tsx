b26633e419aea7bbe44d71cb12a89f3e6c0fb170
RudenkoArt <rudnkoart@gmail.com>
2023-08-07 15:32:54 +0300

    set up react-routing and code-splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import { Routes, Route, Link } from "react-router-dom";
 import {Counter} from "./components/Counter";
 import './index.scss'
+import AboutPage from "./pages/AboutPage/AboutPage";
+import MainPage from "./pages/MainPage/MainPage";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import { Suspense } from "react";
 
 const App = () => {
     return (
         <div className="app">
-            HELOOOO
-            <Counter/>
+            <Link to={'/'}>Главная страница</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/'} element={<MainPageAsync/>} />
+                    <Route path={'/about'} element={<AboutPageAsync/>} />
+                </Routes>
+            </Suspense>
         </div>
     );
 };
