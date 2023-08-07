ea8ae53d53b7c8d86dfecfa12cc445bafce8f90b
RudenkoArt <rudnkoart@gmail.com>
2023-08-07 16:40:29 +0300

    add styles organization & themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { Routes, Route, Link } from "react-router-dom";
-import {Counter} from "./components/Counter";
-import './index.scss'
-import AboutPage from "./pages/AboutPage/AboutPage";
-import MainPage from "./pages/MainPage/MainPage";
+import './styles/index.scss'
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import { Suspense } from "react";
+import {useTheme} from "./theme/useTheme";
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme()
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>Главная страница</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
